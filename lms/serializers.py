6bd5c17f29902639e6bdddac8a0a9325054a1c3f
Aleksandr <zemacohoznik@gmail.com>
2024-11-06 16:18:40 +0300

    Переработка сериализатора курса по заданию

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,14 +3,20 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = serializers.SerializerMethodField
+    lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
+
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.count()
