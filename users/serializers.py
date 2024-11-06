71990a86699dc5a0edbbdd34657b2d8c30f96a61
Aleksandr <zemacohoznik@gmail.com>
2024-11-06 16:19:25 +0300

    Создание сериализатора плажетей и корректировка сериализатора юзера по заданию

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payments
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = "__all__"
+
 
 class UserSerializers(serializers.ModelSerializer):
-        class Meta:
-            model = User
-            fields = '__all__'
+    payments_history = PaymentSerializer(
+        many=True, source="payment_set", read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = "__all__"
