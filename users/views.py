0b4acf4c49fa3151cfe21cfcbac9e7125fb3d091
Aleksandr <zemacohoznik@gmail.com>
2024-11-06 16:20:08 +0300

    Создание контроллеров под платежи

--- users/views.py
+++ users/views.py
@@ -1,23 +1,56 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
-from users.serializers import UserSerializers
+from users.serializers import UserSerializers, PaymentSerializer
 
-from users.models import User
+from users.models import User, Payments
+
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializers
+
 
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializers
     queryset = User.objects.all()
 
+
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializers
     queryset = User.objects.all()
+
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializers
     queryset = User.objects.all()
 
+
 class UserDestroyAPIView(generics.DestroyAPIView):
     queryset = User.objects.all()
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payments.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = (
+        "payment_method",
+        "payment_course",
+        "payment_lesson",
+    )
+    ordering_fields = ("date",)
+
+
+class PaymentRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payments.objects.all()
+
+class PaymentUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payments.objects.all()
