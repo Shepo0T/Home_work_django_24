7cd592cab00b9a3aeebc02d5b3131caee8f3fdbc
Aleksandr <zemacohoznik@gmail.com>
2024-11-10 20:50:08 +0300

    добавление сериализатора подписки и правки сериализатора курса под подписку

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
+from lms.validators import LinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -8,15 +9,26 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
-
+        validators = [
+            LinkValidator(field='video_url')
+        ]
 
 class CourseSerializer(serializers.ModelSerializer):
     number_of_lessons = serializers.SerializerMethodField
     lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
 
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.count()
+
+    def get_subscription(self, course):
+        user = self.context['request'].user
+        return Subscription.objects.all().filter(user=user).filter(course=course).exists()
+
     class Meta:
         model = Course
         fields = "__all__"
+class SubscriptionSerializer(serializers.ModelSerializer):
 
-    def get_lesson_count(self, instance):
-        return instance.lesson_set.count()
+    class Meta:
+        model = Subscription
+        fields = '__all__'
