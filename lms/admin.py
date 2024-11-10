c22feed16894cd055b8845f1083bdce12a7c5dab
Aleksandr <zemacohoznik@gmail.com>
2024-11-10 20:49:11 +0300

    добавление управления в админке

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from lms.models import Subscription
+from users.models import Payments, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели "User" в административной панели"""
+
+    list_display = (
+        "pk",
+        "email",
+    )
+
+
+@admin.register(Payments)
+class PaymentAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели "Payment" в административной панели"""
+
+    list_display = (
+        "pk",
+        "user",
+        "payment_course",
+        "payment_lesson",
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'course',)
