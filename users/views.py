4b8e50ddd382975239c251ddad08a9f72cc4132d
Aleksandr <zemacohoznik@gmail.com>
2024-11-14 13:10:57 +0300

    добавление док-стрингов и метода для платежей

--- users/views.py
+++ users/views.py
@@ -6,9 +6,11 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from users.serializers import UserSerializers, PaymentSerializer
 
 from users.models import User, Payments
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
 
 
 class UserCreateAPIView(generics.CreateAPIView):
+    """Контроллер для создания пользователя"""
     serializer_class = UserSerializers
     queryset = User.objects.all()
     permission_classes = [AllowAny]
@@ -19,30 +21,45 @@ class UserCreateAPIView(generics.CreateAPIView):
         user.save()
 
 class UserListAPIView(generics.ListAPIView):
+    """Контроллер для просмотра списка пользователей"""
     serializer_class = UserSerializers
     queryset = User.objects.all()
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """Контроллер для просмотра одного пользователя"""
     serializer_class = UserSerializers
     queryset = User.objects.all()
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
+    """Контроллер для редактирования пользователя"""
     serializer_class = UserSerializers
     queryset = User.objects.all()
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
+    """Контроллер для удаления пользователя"""
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
 class PaymentCreateAPIView(generics.CreateAPIView):
+    """Контроллер для создания платежей"""
     serializer_class = PaymentSerializer
     queryset = Payments.objects.all()
 
+    def perform_create(self, serializer):
+        """Переопределение метода для возможности оплаты курсов."""
+        payment = serializer.save(user=self.request.user)
+        stripe_product_id = create_stripe_product(payment)
+        price_id = create_stripe_price(payment, stripe_product_id)
+        session_id, session_link = create_stripe_session(price_id)
+        payment.session_id = session_id
+        payment.payment_link = session_link
+        payment.save()
 
 class PaymentListAPIView(generics.ListAPIView):
+    """Контроллер для просмотра списка платежей"""
     serializer_class = PaymentSerializer
     queryset = Payments.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
@@ -55,9 +72,11 @@ class PaymentListAPIView(generics.ListAPIView):
 
 
 class PaymentRetrieveAPIView(generics.RetrieveAPIView):
+    """Контроллер для просмотра одного платежа"""
     serializer_class = PaymentSerializer
     queryset = Payments.objects.all()
 
 class PaymentUpdateAPIView(generics.UpdateAPIView):
+    """Контроллер для редактирования платежа"""
     serializer_class = PaymentSerializer
     queryset = Payments.objects.all()
